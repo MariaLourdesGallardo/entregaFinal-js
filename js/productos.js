69b77dd89717bdefc463f9b381db8e39bb0451c7
Gallardo Maria Lourdes <lourg3022@gmail.com>
2023-01-23 20:15:24 +0100

    fetch

--- js/productos.js
+++ js/productos.js
@@ -12,71 +12,67 @@ const bajarDelLs = (key) =>{
 
 let carrito = []
 
-productosTienda.forEach( (element) =>{
-    const card = document.createElement("div")
-        card.className = "tarjeta text-center"
-        card.innerHTML = `
-            <div class="card " style="width: 12rem;" id=${element.id}>
-                <img src=${element.img} class="card-img-top" alt=${element.producto}
-            </div>
-            <div class="class="card-body ">
-                <div>
-                    ${element.precio} <span>€</span>
-                </div>
-                <h5 class="card-title">${element.marca}</h5>
-                <p class="card-text">${element.producto}</p>
-                <button class="btn btn-primary" id=${element.id} >Añadir al carrito</button>
-            </div>   
-        `
-        boxCards.append(card)
-
-
-
-
-    card.addEventListener("click", (e) =>{
-        if(e.target.classList.contains("btn-primary")){
-            const repeat = carrito.some ( (el)=> el.id === element.id )
-            
-            if (repeat){
-                carrito.map ((prod)=>{
-                    if(prod.id === element.id){
-                        prod.cantidad++
-                    }
-                })
-            }else{
-                carrito.push({
-                    id: element.id,
-                    img: element.img,
-                    marca: element.marca,
-                    nombre: element.producto,
-                    descripcion1: element.descripcion1,
-                    precio: element.precio,
-                    cantidad: element.cantidad
-                    })
-
-                }
-
-
-            subirAlLs("productos",carrito)
-
-
+const tienda = async () =>{
     
-        }
-            
-    })
-})
+    try {
+    const respuesta = await fetch ("https://63ce6953d2e8c29a9bd5f4c8.mockapi.io/api/productos")
+    const data = await respuesta.json()
+    console.log(data)
+    data.forEach( (element) =>{
+        const card = document.createElement("div")
+            card.className = "tarjeta text-center"
+            card.innerHTML = `
+                <div class="card " style="width: 12rem;" id=${element.id}>
+                    <img src=${element.img} class="card-img-top" alt=${element.producto}
+                </div>
+                <div class="class="card-body ">
+                    <div>
+                        ${element.precio} <span>€</span>
+                    </div>
+                    <h5 class="card-title">${element.marca}</h5>
+                    <p class="card-text">${element.producto}</p>
+                    <button class="btn btn-primary" id=${element.id} >Añadir al carrito</button>
+                </div>   
+                     `
+            boxCards.append(card)
+
+
+            card.addEventListener("click", (e) =>{
+                if(e.target.classList.contains("btn-primary")){
+                    const repeat = carrito.some ( (el)=> el.id === element.id )
+                    if (repeat){
+                        carrito.map ((prod)=>{
+                            if(prod.id === element.id){
+                                prod.cantidad++
+                            }
+                        })
+                    }else{
+                        carrito.push({
+                            id: element.id,
+                            img: element.img,
+                            marca: element.marca,
+                            nombre: element.producto,
+                            descripcion1: element.descripcion1,
+                            precio: element.precio,
+                            cantidad: element.cantidad
+                        })  
+
+                    }
+                    subirAlLs("productos",carrito)
+                }
+            })
+
+    } )
+
+    }catch(error){
+        console.log(error)
+    }
+}
+tienda()
+
 
 const carritoModificado = bajarDelLs("productos")
 
 carrito = carritoModificado
 console.log(carritoModificado)
 
-// const repeat = carrito.some ( (el)=> el.id === element.id )
-            
-// if (repeat) {
-//     carrito.map ( (producto) =>{
-//         if (producto.id === element.id){
-//             element.cantidad++
-           
-//         }
-//     } )
